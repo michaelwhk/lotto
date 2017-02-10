ed3df32e46cc90ed35c38bc8982ccc5e52d1fc4e
Michael Wang <wanghk@aceworld.com.au>
2017-02-10 19:11:27 +1100

    add missing number area

--- app/controllers/oz_lottos_controller.rb
+++ app/controllers/oz_lottos_controller.rb
@@ -4,16 +4,18 @@ class OzLottosController < ApplicationController
   respond_to :html, :js
 
   def index
-
+    @vacant_number = vacant_number(@oz_lottos)
   end
 
   def add
     @lotto = generate_lotto
     @oz_lottos.create(n1: @lotto[0], n2: @lotto[1], n3: @lotto[2], n4: @lotto[3], n5: @lotto[4], n6: @lotto[5], n7: @lotto[6])
+    @vacant_number = vacant_number(@oz_lottos)
   end
 
   def remove
     @oz_lotto.destroy
+    @vacant_number = vacant_number(@oz_lottos)
   end
 
 
@@ -29,6 +31,21 @@ class OzLottosController < ApplicationController
 
     def set_lotto
       @oz_lotto = OzLotto.find(params[:id])
+    end
+
+    # def range_count(start,end,lotto)
+    #   array = (start...end).to_a
+    #   a2 = [lotto.n1,lotto.n2,lotto.n3,lotto.n4,lotto.n5,lotto.n6,lotto.n7]
+    #   count_result = (array - (array - a2)).count
+    #   return count_result
+    # end
+    def vacant_number(lottos)
+      pool = (1..45).to_a
+      lottos.each do |l|
+        array = [l.n1,l.n2,l.n3,l.n4,l.n5,l.n6,l.n7]
+        pool = pool - array
+      end
+      return pool
     end
 
     def generate_lotto
--- app/helpers/oz_lottos_helper.rb
+++ app/helpers/oz_lottos_helper.rb
@@ -1,2 +1,22 @@
 module OzLottosHelper
+
+
+
+
+  def range_count(first,last,lotto)
+    array = (first..last).to_a
+    a2 = [lotto.n1,lotto.n2,lotto.n3,lotto.n4,lotto.n5,lotto.n6,lotto.n7]
+    count_result = (array - (array - a2)).count
+    return count_result
+  end
+
+  def vacant_number(lottos)
+    pool = (1..45).to_a
+    lottos.each do |l|
+      pool = pool - l
+    end
+    return pool
+  end
+
+
 end
